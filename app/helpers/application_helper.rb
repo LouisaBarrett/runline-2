471a21695b8fe36b43e9f77e80d890067e4ffa93
Bree Thomas <bree@aha-method.com>
2014-01-30 11:20:05 -0700

    Fix formatting for floats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   class Formatting
-    def self.format_pace(seconds)
-      hours = seconds/3600.to_i
-      minutes = (seconds/60 - hours *  60).to_i
+    def self.format_for(seconds)
+      hours = (seconds/3600).to_i
+      minutes = ((seconds/60) - (hours *  60)).to_i
       seconds_remaining = (seconds - (minutes * 60 + hours * 3600))
       if hours >= 1
         "%01d:%02d:%02d" % [hours, minutes, seconds_remaining]
@@ -15,6 +15,10 @@ module ApplicationHelper
         "%02d:%02d" % [minutes, seconds_remaining]
       end
     end
+
+  def format_for(date)
+    run.workout_datetime.to_time.strftime("%a, %m/%d/%y")
+  end
   end
 
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,25 +3,32 @@ require "spec_helper"
 describe "Formatting" do
   it "formats for seconds that are more than 1 hour" do
     seconds = 4500
-    formatter = ApplicationHelper::Formatting.format_pace(seconds)
+    formatter = ApplicationHelper::Formatting.format_for(seconds)
     expect("1:15:00").to eq(formatter)
   end
- 
+
   it "formats for seconds that are less than 1 hour" do
     seconds = 1500
-    formatter = ApplicationHelper::Formatting.format_pace(seconds)
+    formatter = ApplicationHelper::Formatting.format_for(seconds)
     expect("25:00").to eq(formatter)
   end
 
   it "formats for seconds that are less than 1 minute" do
     seconds = 54
-    formatter = ApplicationHelper::Formatting.format_pace(seconds)
+    formatter = ApplicationHelper::Formatting.format_for(seconds)
     expect("00:54").to eq(formatter)
   end
 
   it "can accept seconds that are integers or floats" do
     seconds = 1159.2
-    formatter = ApplicationHelper::Formatting.format_pace(seconds)
-    expect("19:32")
+    formatter = ApplicationHelper::Formatting.format_for(seconds)
+    expect("19:19").to eq(formatter)
+  end
+
+  xit "formats the workout date time to a readable date" do
+    run = FactoryGirl.create(:run, workout_datetime: "2012-11-03T23:12:49+00:00")
+    formatter = ApplicationHelper::Formatting.format_for(run.workout_datetime)
+    expect("Sat, 11/03/12").to eq(formatter)
+
   end
 end
