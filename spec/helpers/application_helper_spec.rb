a37ef08ac5d3907c2632c7283478b7c9037d28a5
Louisa Barrett <louisabarrett@gmail.com>
2014-01-30 09:12:54 -0700

    format for seconds that less than one hour

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,21 +7,21 @@ describe "Formatting" do
     expect("1:15:00").to eq(formatter)
   end
  
-  xit "formats for seconds that are less than 1 hour" do
+  it "formats for seconds that are less than 1 hour" do
     seconds = 1500
-    formatting = ApplicationHelper::formatting.format_pace(seconds)
-    expect("25:00").to eq(formatting)
+    formatter = ApplicationHelper::Formatting.format_pace(seconds)
+    expect("25:00").to eq(formatter)
   end
 
   xit "formats for seconds that are less than 1 minute" do
     seconds = 54
-    formatting = ApplicationHelper::formatting.format_pace(seconds)
-    expect("00:54").to eq(formatting)
+    formatter = ApplicationHelper::formatting.format_pace(seconds)
+    expect("00:54").to eq(formatter)
   end
 
   xit "can accept seconds that are integers or floats" do
     seconds = 1159.2
-    formatting = ApplicationHelper::formatting.format_pace(seconds)
+    formatter = ApplicationHelper::formatting.format_pace(seconds)
     expect("19:32")
   end
 end
