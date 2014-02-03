e505eba559f8f7f25064129b72add319a5a50166
Louisa Barrett <louisabarrett@gmail.com>
2014-02-03 14:31:43 -0700

    updated compare controller and view

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -9,9 +9,7 @@ class CompareController < ApplicationController
       @diff_pace = (current_user.pace - @friend.pace)
       @diff_longest_run = (current_user.longest_run.miles - @friend.longest_run.miles).round(2)
 
-      @user_total_distance = current_user_total_distance
-      @user_longest_run = current_user_longest_run
-      @user_average_pace = current_user_average_pace
+      @stats = Stats.new(current_user.runs)
 
     end
   end
@@ -27,22 +25,4 @@ class CompareController < ApplicationController
     current_friend.first
   end
 
-  private
-
-  def current_user_total_distance
-    meters = Calculator.new(current_user.runs).total_distance
-    miles = DistanceConverter.new(meters).miles
-    Formatter.new.format_for_miles(miles)
-  end
-
-  def current_user_longest_run
-    meters = Calculator.new(current_user.runs).longest_run
-    miles = DistanceConverter.new(meters).miles
-    Formatter.new.format_for_miles(miles)
-  end
-
-  def current_user_average_pace
-    seconds = Calculator.new(current_user.runs).total_average_pace_per_mile
-    Formatter.new.format_for_seconds(seconds)
-  end
 end
