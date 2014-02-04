14d5673f58df6a40a2ac2c9c1961577a239b65c1
Bree Thomas <bree@aha-method.com>
2014-02-04 12:55:00 -0700

    Remove unused methods from run model

--- app/models/run.rb
+++ app/models/run.rb
@@ -5,21 +5,4 @@ class Run < ActiveRecord::Base
   validates :distance, presence: true
   validates :run_time, presence: true
   validates :workout_datetime, presence: true
-
-  def miles
-    self.distance / 1609.34
-  end
-
-  def format_date
-    ApplicationHelper::Formatting.format_for_date(self.workout_datetime)
-  end
-
-  def time_in_minutes
-    ApplicationHelper::Formatting.format_for_seconds(self.run_time)
-  end
-
-  def mile_pace_in_minutes
-    ApplicationHelper::Formatting.format_for_seconds((self.run_time / self.miles))
-  end
-
 end
