da4038eecfb9c1837fa7b63d6ba5a36e48001f27
Bree Thomas <bree@aha-method.com>
2014-02-05 16:08:39 -0700

    Refactor dash views using application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def total_distance_in_miles_for(stats)
+    stats.total_distance.round(2)
+  end
+
+  def longest_run_in_miles_for(stats)
+    stats.longest_run.round(2)
+  end
+
+  def average_pace_formatted_for(stats)
+    Formatter.new.format_for_seconds(stats.average_pace)
+  end
+
+  def compared_miles_for(diff)
+    diff.miles.round(2)
+  end
+
+  def compared_longest_run_for(diff)
+    diff.longest_run.round(2)
+  end
+
+  def compared_pace_for(diff)
+    Formatter.new.format_for_seconds(diff.pace.abs)
+  end
 end
