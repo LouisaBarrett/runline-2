75ab1e3d9cccd27ef5c80ab8a52ec17351ab6c16
Bree Thomas <bree@aha-method.com>
2014-02-11 14:02:49 -0700

    Fix find_current_friend method for compare view

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,7 +1,7 @@
 class CompareController < ApplicationController
 
   def show
-    @friend = find_current_friend(params[:id])
+    @friend = find_current_friend(params[:id]) 
     unless current_user.runs.empty? || @friend.runs.empty?
       @user_stats = Stats.new(current_user.runs)
       @friend_stats = Stats.new(@friend.runs)
@@ -9,15 +9,11 @@ class CompareController < ApplicationController
     end
   end
 
-  def find_current_friend(id)
-    current_friend = []
-    current_user.total_approved_friends.each do |friend|
-      if friend.id == id.to_i
-        current_friend << friend
-      end
-      current_friend
-    end
-    current_friend.first
+  private
+
+  def find_current_friend(friendship_id) 
+    friendship = Friendship.find(friendship_id)
+    User.find(friendship.requester) || User.find(friendship.receiver)
   end
 
 end
