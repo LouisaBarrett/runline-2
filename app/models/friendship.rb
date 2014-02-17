d0f9bdbdcc178687294c883a01a15d835fa7fe2a
Louisa Barrett <louisabarrett@gmail.com>
2014-02-17 16:23:25 -0700

    Temporary disable associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,10 @@
 class Friendship < ActiveRecord::Base
   attr_accessor :friend_name
 
-  belongs_to :requester, :primary_key => "id", :class_name => "User", :foreign_key => "requester"
-  belongs_to :receiver, :primary_key => "id", :class_name => "User", :foreign_key => "receiver"
+  # belongs_to :requester, :primary_key => "id", :class_name => "User", :foreign_key => "requester"
+  # belongs_to :receiver, :primary_key => "id", :class_name => "User", :foreign_key => "receiver"
   
-  belongs_to :friend, :class_name => "User"
+  # belongs_to :friend, :class_name => "User"
 
 
   validates :requester, presence: true
