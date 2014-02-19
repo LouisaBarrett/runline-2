929f455854234e2b056e56c12de97ecf533df8f3
Louisa Barrett <louisabarrett@gmail.com>
2014-02-19 14:00:08 -0700

    refactor dashboards_controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -14,7 +14,24 @@ class DashboardsController < ApplicationController
   end
 
   def fetch_runs
-    current_user.fetch_runs
+    recent_runs
     redirect_to dashboard_path
   end
+
+  private
+
+  def recent_runs
+    store = MapMyFitness::WorkoutStore.new(current_user.token)
+    runs = store.workouts_by_user_in_last_days(current_user.uid, 14)
+    runs.each do |run|
+      Run.where(:mmf_identifier => run.id).first_or_create(
+        user_id: User.find_by_uid(current_user.uid).id,
+        name: run.name,
+        distance: run.distance,
+        run_time: run.duration,
+        workout_datetime: run.started_at 
+      )
+    end
+  end
+
 end
