34899af37c2105cf4fd280a0ffd8275c6a6ab6ce
Bree Thomas <bree@aha-method.com>
2014-02-19 12:05:30 -0700

    Fix requester/receiver with _id

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,11 +1,10 @@
 class Friendship < ActiveRecord::Base
   attr_accessor :friend_name
 
-  # belongs_to :requester, :primary_key => "id", :class_name => "User", :foreign_key => "requester"
-  # belongs_to :receiver, :primary_key => "id", :class_name => "User", :foreign_key => "receiver"
-  
-  # belongs_to :friend, :class_name => "User"
+  belongs_to :requester, :primary_key => "id", :class_name => "User", :foreign_key => "requester_id"
+  belongs_to :receiver, :primary_key => "id", :class_name => "User", :foreign_key => "receiver_id"
 
+  belongs_to :friend, :class_name => "User"
 
   validates :requester_id, presence: true
   validates :receiver_id, presence: true
