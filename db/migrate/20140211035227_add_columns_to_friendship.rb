5d667b9844171e9a13c081ab3eb4ae148dcff59f
Katrina Owen <katrina.owen@gmail.com>
2014-02-19 15:50:55 -0700

    Delete duplicate and orphan migrations

--- db/migrate/20140211035227_add_columns_to_friendship.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToFriendship < ActiveRecord::Migration
-  def change
-    add_column :friendships, :requester, :integer
-    add_column :friendships, :receiver, :integer
-  end
-end
--- db/migrate/20140211035516_drop_friendship_temp.rb
+++
@@ -1,5 +0,0 @@
-class DropFriendshipTemp < ActiveRecord::Migration
-  def change
-    drop_table :friendship_temps
-  end
-end
