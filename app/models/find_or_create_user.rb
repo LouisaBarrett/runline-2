92a6e7dbad93ce0c89121727ba2d6c3fe93f3134
Louisa Barrett <louisabarrett@gmail.com>
2014-02-20 14:52:03 -0700

    Mike notes

--- app/models/find_or_create_user.rb
+++ app/models/find_or_create_user.rb
@@ -1,3 +1,8 @@
+# class Authentication < AR::Base
+#   validates :provider ...
+#   belongs_to :user
+# end
+
 class FindOrCreateUser
  
   def self.find_or_create_by_auth(user_data)
@@ -6,6 +11,29 @@ class FindOrCreateUser
       email: user_data.email,
       token: user_data.token
     )
+
+    # if Authentication.where(provider: user_data.provider, uid: user_data.uid).exist?
+    #   auth = Authentication.where(provider: user_data.provider, uid: user_data.uid).first
+    #   auth.user
+    # else
+    #   # AR.transaction do
+    #     auth = Authentication.create(provider: user_data.provider, uid: user_data.uid)
+    #     auth.user.create(
+    #       username: user_data.first_name + " " + user_data.last_name,
+    #       email: user_data.email,
+    #       token: user_data.token
+    #     )
+    #   # end
+    #   # bad
+    #   # auth.destroy unless user.persisted?
+    #   # user = User.create
+    #   # user.authentications << auth
+    # end
   end
 
 end
+
+# TRANSACTION
+#   INSERT INTO users ('Mike')
+#   INSERT INTO users ('Louisa')
+# END
