51bb6f7487908680f816aa82576b4a541495f9ce
Bree Thomas <bree@aha-method.com>
2014-02-26 16:41:03 -0700

    Clean out old comments/notes

--- app/models/find_or_create_user.rb
+++ app/models/find_or_create_user.rb
@@ -1,23 +1,14 @@
 class FindOrCreateUser
- 
   def self.find_or_create_by_auth(user_data)
-    # User.where(:provider => user_data.provider, :uid => user_data.uid).first_or_create(
-    #   username: user_data.first_name + " " + user_data.last_name,
-    #   email: user_data.email,
-    #   token: user_data.token
-    # )
-
     if Authentication.where(provider: user_data.provider, uid: user_data.uid).exists?
       auth = Authentication.where(provider: user_data.provider, uid: user_data.uid).first
       auth.user
     else
       ActiveRecord::Base.transaction do
         new_user = User.find_or_create_by(
-         username: user_data.first_name + " " + user_data.last_name,
-         email: user_data.email
-         # token: user_data.token
-         )
-        # NOTE: user_id was moved out to get this working, needs to be refactored?
+          username: user_data.first_name + " " + user_data.last_name,
+          email: user_data.email
+        )
         auth = Authentication.new(provider: user_data.provider, uid: user_data.uid, token: user_data.token)
         auth.user_id = new_user.id
         auth.save
@@ -26,26 +17,3 @@ class FindOrCreateUser
     end
   end
 end
-
-    # if Authentication.where(provider: user_data.provider, uid: user_data.uid).exist?
-    #   auth = Authentication.where(provider: user_data.provider, uid: user_data.uid).first
-    #   auth.user
-    # else
-    #   # AR.transaction do
-    #     auth = Authentication.create(provider: user_data.provider, uid: user_data.uid)
-    #     auth.user.create(
-    #       username: user_data.first_name + " " + user_data.last_name,
-    #       email: user_data.email,
-    #       token: user_data.token
-    #     )
-    #   # end
-    #   # bad
-    #   # auth.destroy unless user.persisted?
-    #   # user = User.create
-    #   # user.authentications << auth
-    # end
-
-# TRANSACTION
-#   INSERT INTO users ('Mike')
-#   INSERT INTO users ('Louisa')
-# END
