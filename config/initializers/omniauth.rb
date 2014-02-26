7ce23229338eae4a7108fdb2159bd94eb8ad2d07
Bree Thomas <bree@aha-method.com>
2014-02-26 16:40:49 -0700

    Add provider in initializers omniauth builder

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 # OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :mapmyfitness, ENV['MMF_API_KEY'], ENV['MMF_API_SECRET']
+  provider :strava, ENV['STRAVA_KEY'], ENV['STRAVA_SECRET']
 end
